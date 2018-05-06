3b2760ca8b3afb0563f8e1e8772706066c51082b
Maria Bychkova <masha.by4kova@mail.ru>
2018-05-06 11:26:32 +0300

    Fixed animation and layuot. Added 404 page

--- js/main-js.js
+++ js/main-js.js
@@ -3,34 +3,34 @@ $(document).ready(function(){
 	/*scroll btn time out*/
 	setTimeout ("$('.banner .scroll').fadeIn('slow');",2000);
 
-	/*page animation*/
+/*	page animation
 	$(document).on('click', '.banner .scroll', function(){
 		$('.main-screen').show();
 		setTimeout ("$('.hidden-tittle').show();",800);
 		$('.background-img').show();
-		setTimeout ("$('.banner').hide();",1000);
-	})
-
+		//setTimeout ("$('.banner').hide();",1000);
+	});*/
+	
 	/*btn to main-screen*/
 	$(document).on('click', '.banner .scroll', function(event){
 		event.preventDefault();
 		var id = $(this).attr('href');
 		var top = $(id).offset().top;
 		$('body,html').animate({scrollTop: top}, 1000);
-	})
+	});
 
 	/*location and contacts btn*/
 	$(document).on('click', '.nav-btn', function(){
 		$(this).prev().show('slow');
 		$(this).hide('slow');
-	})
+	});
 
 	/*close side-menu btn*/
 	$(document).on('click', '.close', function(){
 		$(this).parent().hide('slow');
 		$(this).parent().next().show('slow');
 
-	})
+	});
 
 	/*click out of nav*/
 	$(document).mouseup(function(e){
@@ -40,11 +40,5 @@ $(document).ready(function(){
 				$('.nav-btn').show();
 				}
 	});
-
+	
 });
-/*		$(window).scroll(function(){
-	var scroll = $(this).scrollTop();
-	$('.main-screen').css({
-		"transform" : "translate(0%, -"+scroll/100+"%"
-	})
-})*/
